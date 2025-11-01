1b2a0c2c028f8362bdeebf99fc94b29454d055b8
iamjothees <iamjothees@gmail.com>
2025-11-01 23:14:05 +0530

    refactor(ui): extract layout component from bootstrapping and enhance error handling
    - Extract Layout component from Bootstrapping for better reusability
    - Add transition effects and improve error message display in BootstrapError

--- frontend/app/shared/components/ui/bootstrapError.jsx
+++ frontend/app/shared/components/ui/bootstrapError.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+import { Layout } from "./bootstrapping";
+import { cn } from "@/shared/lib/utils";
+
 export default function BootstrapError() {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => setShow(true),[]);
     return (
-        <div>Error</div>
+        <Layout>
+            <div className={cn("text-center transition-all duration-1000 ease-in", show ? "opacity-100" : "opacity-0")}>
+                <p className="display-text text-red-500 mb-2"> Something went wrong! Please restart the app </p>
+                <p className="display-text text-xs text-red-300"> If the problem persists, please contact support </p>
+            </div>
+        </Layout>
+        
     )
 }
--- frontend/app/shared/components/ui/bootstrapping.jsx
+++ frontend/app/shared/components/ui/bootstrapping.jsx
@@ -1,11 +1,18 @@
 import logo from '@/../public/logo.png';
 
 export default function Bootstrapping() {
+    return <Layout />
+}
+
+export const Layout = ({ children }) => {
     return (
         <section className='h-screen w-full'>
-            <main className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center`}>
+            <main className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-1000`}>
+            <div className="flex flex-col items-center">
                 <img src={logo} alt={"logo"} height={200} width={200} />
+                {children}
+            </div>
             </main>
         </section>
-    )
+    );
 }
